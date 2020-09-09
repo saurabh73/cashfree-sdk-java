35e0a7a1303f42cbf0ca53f10741a56be9421d60
Saurabh Dutta <saurabhdutta.jk@gmail.com>
2020-09-10 00:27:14 +0100

    Fix Getter Setter for Order Token Request

--- src/main/java/com/cashfree/lib/pg/domains/request/OrderTokenRequest.java
+++ src/main/java/com/cashfree/lib/pg/domains/request/OrderTokenRequest.java
@@ -1,9 +1,13 @@
 package com.cashfree.lib.pg.domains.request;
 
 import com.cashfree.lib.annotations.NotNull;
+import lombok.Data;
+import lombok.experimental.Accessors;
 
 import java.math.BigDecimal;
 
+@Data
+@Accessors(chain = true)
 public class OrderTokenRequest {
 
     @NotNull
